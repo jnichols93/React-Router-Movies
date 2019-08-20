6c9303ed312c6b34b3c16e7ed68c03f3c9d45f6e
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-08-20 12:17:03 -0500

    MVP hit

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,36 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const MovieList = props => {
-  const [movies, setMovies] = useState([])
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovies(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    
-    getMovies();
-  }, []);
-  
-  return (
-    <div className="movie-list">
-      {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </div>
-  );
+export default class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/movies')
+      .then(response => {
+        this.setState(() => ({ movies: response.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="movie-list">
+        {this.state.movies.map(movie => (
+          <MovieDetails key={movie.id} movie={movie} />
+        ))}
+      </div>
+    );
+  }
 }
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    <div className="movie-card"> <Link to={`/movies/${movie.id}`} className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -45,8 +50,6 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
-    </div>
+    </Link> </div>
   );
 }
-
-export default MovieList;
